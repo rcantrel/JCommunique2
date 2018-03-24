9f1fec58676e87d39b77754d9d53b9a4777bff36
Randall Cantrell <rcantrel@gmail.com>
2018-03-24 11:49:20 -0400

    update

--- src/main/java/com/utils/Screen.java
+++ src/main/java/com/utils/Screen.java
@@ -4,7 +4,6 @@ import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
-import java.awt.Window;
 
 import com.notification.Notification;
 import com.notification.NotificationFactory;
@@ -12,30 +11,24 @@ import com.platform.Mac;
 import com.platform.Platform;
 
 public class Screen {
-	private int m_width = 0;
-	private int m_height = 0;
+	private int mWidth = 0;
+	private int mHeight = 0;
 
-	private int m_leftX = 0;
-	private int m_centerX = 0;
-	private int m_rightX = 0;
+	private int mLeftX = 0;
+	private int mCenterX = 0;
+	private int mRightX = 0;
 
-	private int m_topY = 0;
-	private int m_centerY = 0;
-	private int m_bottomY = 0;
+	private int mTopY = 0;
+	private int mCenterY = 0;
+	private int mBottomY = 0;
 
-	private int m_padding = 0;
+	private int mPadding = 0;
 
 	private Platform platform = new Platform();
 
 	private Screen(boolean spanMultipleMonitors, int padding) {
-		m_padding = padding;
+		mPadding = padding;
 		setupDimensions(spanMultipleMonitors);
-		calculatePositions();
-	}
-
-	private Screen(Window window, int padding) {
-		m_padding = padding;
-		setupDimensions(window);
 		calculatePositions();
 	}
 
@@ -58,66 +51,44 @@ public class Screen {
 	private void setupDimensions(boolean spanMultipleMonitors) {
 		if (spanMultipleMonitors) {
 			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-			m_width = screenSize.width;
-			m_height = screenSize.height;
+			mWidth = screenSize.width;
+			mHeight = screenSize.height;
 
 		} else {
 			GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-			m_width = gd.getDisplayMode().getWidth();
-			m_height = gd.getDisplayMode().getHeight();
+			mWidth = gd.getDisplayMode().getWidth();
+			mHeight = gd.getDisplayMode().getHeight();
 		}
 	}
 
-	private void setupDimensions(Window window) {
-		return;
-		//		GraphicsConfiguration config = window.getGraphicsConfiguration();
-		//		GraphicsDevice myScreen = config.getDevice();
-		//		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		//		GraphicsDevice[] allScreens = env.getScreenDevices();
-		//		for (int i = 0; i < allScreens.length; i++) {
-		//			m_width += allScreens[i].getDisplayMode().getWidth();
-		//			m_height += allScreens[i].getDisplayMode().getHeight();
-		//			if (allScreens[i].equals(myScreen)) {
-		//				break;
-		//			}
-		//		}
-		//
-		//		if(m_width == 0 && m_height == 0) {
-		//			System.out.println("Unable to determine screen size");
-		//			setupDimensions(true);
-		//		} else {
-		//			System.out.println(m_width + "  " + m_height);
-		//		}
-	}
-
 	private void calculatePositions() {
-		m_leftX = m_padding;
-		m_centerX = (int) (m_width / 2d);
-		m_rightX = m_width - m_padding;
+		mLeftX = mPadding;
+		mCenterX = (int) (mWidth / 2d);
+		mRightX = mWidth - mPadding;
 
-		m_topY = m_padding;
-		m_centerY = (int) (m_height / 2d);
-		m_bottomY = m_height - m_padding - (platform.getOperatingSystem() instanceof Mac ? 50 : 0);
+		mTopY = mPadding;
+		mCenterY = (int) (mHeight / 2d);
+		mBottomY = mHeight - mPadding - (platform.getOperatingSystem() instanceof Mac ? 50 : 0);
 	}
 
 	public int getX(String loc, Notification note) {
 		switch (loc) {
 		case NotificationFactory.SOUTHWEST:
-			return m_leftX;
+			return mLeftX;
 		case NotificationFactory.WEST:
-			return m_leftX;
+			return mLeftX;
 		case NotificationFactory.NORTHWEST:
-			return m_leftX;
+			return mLeftX;
 		case NotificationFactory.NORTH:
-			return m_centerX - note.getWidth() / 2;
+			return mCenterX - note.getWidth() / 2;
 		case NotificationFactory.SOUTH:
-			return m_centerX - note.getWidth() / 2;
+			return mCenterX - note.getWidth() / 2;
 		case NotificationFactory.SOUTHEAST:
-			return m_rightX - note.getWidth();
+			return mRightX - note.getWidth();
 		case NotificationFactory.EAST:
-			return m_rightX - note.getWidth();
+			return mRightX - note.getWidth();
 		case NotificationFactory.NORTHEAST:
-			return m_rightX - note.getWidth();
+			return mRightX - note.getWidth();
 		default:
 			return -1;
 		}
@@ -126,27 +97,27 @@ public class Screen {
 	public int getY(String loc, Notification note) {
 		switch (loc) {
 		case NotificationFactory.SOUTHWEST:
-			return m_bottomY - note.getHeight();
+			return mBottomY - note.getHeight();
 		case NotificationFactory.WEST:
-			return m_centerY - note.getHeight() / 2;
+			return mCenterY - note.getHeight() / 2;
 		case NotificationFactory.NORTHWEST:
-			return m_topY;
+			return mTopY;
 		case NotificationFactory.NORTH:
-			return m_topY;
+			return mTopY;
 		case NotificationFactory.SOUTH:
-			return m_bottomY - note.getHeight();
+			return mBottomY - note.getHeight();
 		case NotificationFactory.SOUTHEAST:
-			return m_bottomY - note.getHeight();
+			return mBottomY - note.getHeight();
 		case NotificationFactory.EAST:
-			return m_centerY - note.getHeight() / 2;
+			return mCenterY - note.getHeight() / 2;
 		case NotificationFactory.NORTHEAST:
-			return m_topY;
+			return mTopY;
 		default:
 			return -1;
 		}
 	}
 
 	public int getPadding() {
-		return m_padding;
+		return mPadding;
 	}
 }
